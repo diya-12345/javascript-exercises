7f6519137eb4f0730277444dbe4b73fff9c4935f
diyakadakia <kadakiadiya@gmail.com>
2024-07-02 17:45:37 -0400

    sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,16 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+
+    let sum = 0;
+    if (min > max) [min, max] = [max, min];
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
+
+
 
 };
 
