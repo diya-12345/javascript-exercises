8b0de87de3692752c807fc9d205e18060f3723e8
diyakadakia <kadakiadiya@gmail.com>
2024-07-02 17:57:52 -0400

    leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0; 
+    const isYearDivisiblebyOneHundrend = year % 100 === 0; 
+    const isYearDivisiblebyFourHundrend = year % 400 === 0;
+
+    if (
+        isYearDivisibleByFour && 
+        (!isYearDivisiblebyOneHundrend || isYearDivisiblebyFourHundrend)
+    ) {
+        return true; 
+    } else {
+        return false;
+    } 
 
 };
 
